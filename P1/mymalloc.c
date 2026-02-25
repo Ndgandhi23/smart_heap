d52b8544e39b05604ea27d67141185e647b1c5f2
Asmi D. Narsay <adn54@ilab3.cs.rutgers.edu>
2026-02-24 20:18:20 -0500

    mymalloc.c cleanup

--- P1/mymalloc.c
+++ P1/mymalloc.c
@@ -10,8 +10,6 @@
 static int heap_init = 0;
 
 
-// already declares the heap globally but requires initializing 
-    // aka setting the bytes equal to free bc right now they hold random info
 static union { 
     char bytes[MEMLENGTH]; // 4096 indices aka bytes
     double not_used;
@@ -46,12 +44,9 @@ static void my_little_leaky(void){
     }
 }
 
-
-// initializes the heap to 0 or whatever is equal to a free space
 static void initilize_heap(){
     
   //  printf("\nASMI DEGUB: In initialize_heap()");
-    // atexit(leak_detector);
     memset(heap.bytes, '\0', MEMLENGTH);
 
     // creates the first header with the rest of the heap being its free chunk
@@ -74,7 +69,6 @@ void * mymalloc (size_t size, char *file, int line){
     }
 
     int roundedSize = (size + 7) & ~7; //rounds size to a multiple of 8
-    //printf("\ntest %d's rounded size: %d",line,roundedSize);
 
     struct header headerToAllocate; // header we want to put into heap
     headerToAllocate.size = roundedSize;
@@ -100,18 +94,17 @@ void * mymalloc (size_t size, char *file, int line){
                 memcpy((struct header*)(heap.bytes + i + sizeof(struct header) + roundedSize), &newHeader, sizeof(struct header)); // put newHeader into heap.bytes
             }
             
-            result = (void*)(heap.bytes + i + sizeof(headerToAllocate)); // pointer to the payload
+            result = (void*)(heap.bytes + i + sizeof(struct header)); // pointer to the payload
             break;
         }
 
-        i += (headerToCheck.size + sizeof(headerToCheck)); // i jumps to next header
+        i += (headerToCheck.size + sizeof(struct header)); // i jumps to next header
     }
 
     if (result == NULL){
         fprintf(stderr, "malloc: Unable to allocate %zu bytes (%s:%d)\n", size, file, line);
     }   
 
-   // printf("\nASMI DEBUG: returning result pointer: %p \n", result);
     return result; 
 }
 
@@ -127,16 +120,14 @@ bool ptrExists(void *ptr){
         
         tempPtr = (struct header*)(heap.bytes + i); 
 
-        if(tempPtr == ptr-sizeof(struct header)){
-           // printf("\nASMI DEBUG: found the pointer %p at position %d \n", tempPtr, i);
+        if(tempPtr == ptr-sizeof(struct header)){ // found the pointer
             return true;
         }
 
         memcpy(&headerToCheck, (struct header*)(heap.bytes + i), sizeof(struct header));
-        i += (headerToCheck.size + sizeof(headerToCheck)); // i jumps to next header
+        i += (headerToCheck.size + sizeof(headerToCheck)); 
     }
 
-   // printf("\nASMI DEBUG: did not find the pointer %p anywhere\n", ptr);
     return false;
 }
 
@@ -146,33 +137,13 @@ void myfree (void *ptr, char *file, int line){
         initilize_heap();
     }
 
-    // myfree should merge adjacent free chunks together so we dont need to worry about that in mymalloc
-
-    // step 1
-        // free the current pointer
-        // header is current pointer - 8
-        // merge with the free one after
-            // delete header that is within free chunk
-
-    // step 2
-        // traverse through array going forward and merge any adjacent chunk before current free one
-            // delete header that is within free chunk    
-     
-
-    // CHECK IF POINTER EXISTS
-    if(!ptrExists(ptr)){
+    if(!ptrExists(ptr)){ // checks to see if the pointer is correct and exists as a header
         fprintf(stderr, "free: Inappropriate pointer (%s:%d)\n", file, line);
         exit(2);
     }
 
-    //printf("\nASMI DEBUG: in test %d",line);
-
-    // gets the header of the current pointer
-
-    //     struct header currHead = *(struct header *)((char *)ptr - sizeof(struct header));
-
     struct header currHead; 
-    memcpy(&currHead, ptr-sizeof(struct header), sizeof(struct header));
+    memcpy(&currHead, (char*)(ptr-sizeof(struct header)), sizeof(struct header));
 
     if(currHead.freed == 1){ // freeing a pointer that is already free
         fprintf(stderr, "free: Inappropriate pointer (%s:%d)\n", file, line);
@@ -181,34 +152,22 @@ void myfree (void *ptr, char *file, int line){
 
     currHead.freed = 1;
     memset(ptr, '\0', currHead.size);
-    memcpy(ptr-sizeof(struct header), &currHead, sizeof(struct header));
+    memcpy((char*)(ptr-sizeof(struct header)), &currHead, sizeof(struct header));
 
     // coalescing: 
 
     // checks to see if the chunk after the current one if free as well
     if((char*)(ptr + currHead.size) <= (char*)(heap.bytes + MEMLENGTH - sizeof(struct header))){ // checks if a next header exists
-      //  printf("\nASMI DEBUG: in if for nextHead");
-        //struct header nextHead = *(struct header*)((char*)ptr + currHead.size);
         struct header nextHead;
         memcpy(&nextHead,ptr+currHead.size,sizeof(struct header));
         if(nextHead.freed == 1){
-        //    printf("\nASMI DEBUG: coelating nextHead to currHead\n");
-            //memset((char*)(ptr + currHead.size), '\0',nextHead.size + sizeof(struct header)); // clears data
             currHead.size += (sizeof(struct header) + nextHead.size); // increments size of header thats first
-            memcpy(ptr - sizeof(struct header), &currHead, sizeof(struct header)); // copies header back to heap
-        //    printf("\nASMI DEBUG: size of combined next ptr: %d",currHead.size);
+            memcpy((char*)(ptr - sizeof(struct header)), &currHead, sizeof(struct header)); // copies header back to heap
         }
     }
-  //  printf("\nASMI DEBUG: after next coelating: \n");
-   // traverse_heap();
 
-    printf("Heap Start: %p\n", (void*)heap.bytes);
-    printf("Target Head: %p\n", (void*)((char*)ptr - sizeof(struct header)));
-
-    #if 1
-    // original: ((void*)heap.bytes < (ptr - sizeof(struct header)))
+    // checks to see if the chunk before is free as well
     if((char *)ptr - sizeof(struct header) > (char *)heap.bytes){
-      //  printf("\nASMI DEBUG: in if for prevHead");
         struct header *currPtr;         
         struct header *prevPtr = (struct header*)(heap.bytes);
         struct header prevHeader;
@@ -219,9 +178,8 @@ void myfree (void *ptr, char *file, int line){
             currPtr = (struct header*)(heap.bytes + i); 
             memcpy(&currHead, currPtr, sizeof(struct header));
 
-            if(currPtr == ptr-sizeof(struct header)){
-                if(prevHeader.freed == 1){
-                //    printf("\nASMI DEBUG: coelating prevHead to currHead\n");
+            if(currPtr == ptr-sizeof(struct header)){ // finds the header before the current one
+                if(prevHeader.freed == 1){ // checks to see if that is a free chunk
                     prevHeader.size += (sizeof(struct header) + currHead.size);
                     memcpy(prevPtr,&prevHeader,sizeof(struct header));
                 }
@@ -230,13 +188,9 @@ void myfree (void *ptr, char *file, int line){
 
             prevPtr = currPtr;
             memcpy(&prevHeader,prevPtr,sizeof(struct header));            
-            i += (currHead.size + sizeof(struct header)); // i jumps to next header
+            i += (currHead.size + sizeof(struct header)); 
         }
     }
-    #endif
-
-  //  printf("\nASMI DEBUG: after prev coelating: \n");
-    //traverse_heap();
 
 }
 
